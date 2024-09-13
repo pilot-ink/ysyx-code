60414d89aff451a4452727f55e4e2e3c87fd8296
tracer-ysyx <tracer@ysyx.org>
2024-09-13 11:30:43 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:30:43 up 19:45,  1 user,  load average: 0.00, 0.02, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+//用来生成.vcd文件
+#include <verilated_vcd_c.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char** argv)
+{
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+	Vtop* top = new Vtop{contextp};
+	
+	VerilatedVcdC *tfp = new VerilatedVcdC; //初始化VCD对象指针
+	contextp->traceEverOn(true);			//打开追踪
+	top->trace(tfp, 0);
+	tfp->open("wave.vcd");
+
+	while(!contextp->gotFinish()){
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+		printf("a = %d, b = %d, c = %d\n", a, b, top->f);
+
+		tfp->dump(contextp->time());		//dump wave
+		contextp->timeInc(1);			//dump wave
+
+		assert(top->f == (a ^ b));
+	}
+	delete top;
+	tfp->close();
+	delete contextp;
+	return 0;
 }
