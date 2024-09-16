288974c6b7c973b0c3c383a5222f97d669738a51
tracer-ysyx <tracer@ysyx.org>
2024-09-16 17:59:39 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:59:39 up  2:41,  1 user,  load average: 0.07, 0.04, 0.00

--- npc/vsrc/ALU4.v
+++ npc/vsrc/ALU4.v
@@ -16,7 +16,7 @@ module ALU4(
 	always @(*)begin
 		case(c)
 			3'b000:begin {carry, result} = a + b + cin; overflow = (a[3] == b[3])&&(result[3] != a[3]);end
-			3'b001:begin t_no_Cin = {4{ cin }}^B; {carry, result} = a + b + cin; overflow = (a[3] == b[3])&&(result[3] != a[3]);end
+			3'b001:begin t_no_Cin = {4{ cin }}^b; {carry, result} = a + b + cin; overflow = (a[3] == b[3])&&(result[3] != a[3]);end
 			3'b010: result = ~a;
 			3'b011: result = a & b;
 			3'b100: result = a | b;
