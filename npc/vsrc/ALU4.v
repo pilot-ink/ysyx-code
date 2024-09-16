f1d1700aba8422f760cd33a41715b85ff0687d30
tracer-ysyx <tracer@ysyx.org>
2024-09-16 18:09:30 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:30 up  2:51,  1 user,  load average: 0.02, 0.03, 0.00

--- npc/vsrc/ALU4.v
+++ npc/vsrc/ALU4.v
@@ -8,7 +8,9 @@ module ALU4(
 
 
 
-	wire result_wire, size_wire,t_no_Cin;
+	wire result_wire, size_wire;
+	wire [3:0] t_no_cin;
+	assign t_no_cin = {4{cin}}^b;
 
 	assign result_wire = result;
 	assign size_wire = zero;
@@ -16,18 +18,18 @@ module ALU4(
 	always @(*)begin
 		case(c)
 			3'b000:begin {carry, result} = a + b + cin; overflow = (a[3] == b[3])&&(result[3] != a[3]);end
-			3'b001:begin t_no_Cin = {4{cin}}^b; {carry, result} = a + t_no_Cin + {3'b000, cin}; overflow = (a[3] == b[3])&&(result[3] != a[3]);end
+			3'b001:begin {carry, result} = a + t_no_Cin + {3'b000, cin}; overflow = (a[3] == b[3])&&(result[3] != a[3]);end
 			3'b010: result = ~a;
 			3'b011: result = a & b;
 			3'b100: result = a | b;
 			3'b101: result = a ^ b;
 			3'b110: begin
-				t_no_Cin = {4{cin}}^b; {carry, result} = a + t_no_Cin + {3'b000, cin}; overflow = (a[3] == b[3])&&(result[3] != a[3]);zero = ~(|result);
+				{carry, result} = a + t_no_Cin + {3'b000, cin}; overflow = (a[3] == b[3])&&(result[3] != a[3]);zero = ~(|result);
 				if(overflow) size = ~result_wire[3];
 				else	size = result_wire[3];
 			end
 			3'b111: begin 
-				t_no_Cin = {4{cin}}^b; {carry, result} = a + t_no_Cin + {3'b000, cin}; overflow = (a[3] == b[3])&&(result[3] != a[3]);zero = ~(|result);
+				{carry, result} = a + t_no_Cin + {3'b000, cin}; overflow = (a[3] == b[3])&&(result[3] != a[3]);zero = ~(|result);
 				size = zero;
 				end
 		endcase
