ecbc2f0093c4feaf54ac3b99ceae6fe076e6db2d
tracer-ysyx <tracer@ysyx.org>
2024-09-16 17:58:41 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:58:41 up  2:40,  1 user,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/ALU4.v
+++ npc/vsrc/ALU4.v
@@ -6,8 +6,7 @@ module ALU4(
 	output reg zero, overflow, carry, size,	//比较大小时，size=1,a>b;size=0,a<b;判断相等时,size=1,a=b;size=0,a不等于b;
 );
 
-	add4 a1 (.a(a), .b(b), .cin(cin), .result(result), .overflow(overflow), .carry(carry));
-	sub4 s1 (.a(a), .b(b), .cin(cin), .result(result), .overflow(overflow), .carry(carry) ,.zero(zero));
+
 
 	wire result_wire, size_wire,t_no_Cin;
 
@@ -34,38 +33,5 @@ module ALU4(
 		endcase
 	end
 
-
-endmodule
-
-
-
-module add4(
-	input [3:0] a, b,
-	output [3:0] result,		//运算结果
-	input cin,				//进位
-	output carry,	  		//进位位
-	output overflow
-);
-	
-	assign {carry, result} = a + b + cin;
-	assign overflow = (a[3] == b[3])&&(outs[3] != a[3]);
-
-endmodule
-
-
-module sub4(
-	input [3:0] a, b,
-	output [3:0] result,          //运算结果
-	input cin,                    //进位
-	output carry,           //进位位
-	output overflow,
-	output zero
-);
-	
-	wire result_wire, t_no_Cin;
-	add4 a1 (.a(a), .b(b), .cin(cin), .result(result), .overflow(overflow), .carry(carry));
-     	assign result_wire = result;
-	assign t_no_Cin = {n{ Cin }}^B;
-	assign zero = ~(|result_wire);
 
 endmodule
