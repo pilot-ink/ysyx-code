4ba337cff90058756f2820752a90d6114ee7c37a
tracer-ysyx <tracer@ysyx.org>
2024-09-30 15:33:45 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:45 up  1:30,  1 user,  load average: 0.66, 0.58, 0.49

--- npc/vsrc/shift_register.v
+++ npc/vsrc/shift_register.v
@@ -20,10 +20,12 @@ module LFSR(
 	output [7:0] result
 );
 		
-	wire in;
-	assign in = result[4] ^ result[3] ^ result[2] ^ result[0];
+	reg in;
+
 	
-	sr s1(.in(in), .q(result));
+	always @(clock) begin
+		in = result[4] ^ result[3] ^ result[2] ^ result[0];
+	end
 	
 
 endmodule
