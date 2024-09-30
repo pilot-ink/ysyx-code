e290eaa98a11c8486afd6abc6f325cd8009a7c77
tracer-ysyx <tracer@ysyx.org>
2024-09-30 14:33:38 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:33:38 up 30 min,  1 user,  load average: 0.10, 0.09, 0.09

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,12 +1,11 @@
 module top(
-	input in,
        input clock,     
        output [7:0] result,
        output [3:0] h1, h2
 );
 	
 	
-	LFSR a1 (.in(in), .clock(clock), .result(result));
+	LFSR a1 (.clock(clock), .result(result));
 	bcd7se b1(.b(result[3:0]), .h(h1));
 	bcd7se b2(.b(result[7:4]), .h(h2));
 
