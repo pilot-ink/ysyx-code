5eb0f566f41979dd142d0b02f3325c96107b9871
tracer-ysyx <tracer@ysyx.org>
2024-10-05 20:14:37 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:37 up 1 day, 37 min,  1 user,  load average: 0.07, 0.04, 0.00

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,33 @@
 module top(
-       input clock,    
-       output [7:0] result,
-       output [6:0] h1, h2
+     input clk,clrn,ps2_clk,ps2_data,
+     input nextdata_n,  
+     output [7:0] data,
+     output reg ready,
+	output reg overflow,
+	output [6:0] h1,h2
 );
-	wire [7:0] result1;
 	
-	assign result1 = result;
+	parameter [31:0] clock_period = 10;
 	
-	LFSR a1 (.clock(clock), .result(result));
-	bcd7seg b1(.b(result1[3:0]), .h(h1));
-	bcd7seg b2(.b(result1[7:4]), .h(h2));
+	
+	initial begin /* clock driver */
+    	clk = 0;
+    	forever
+        	#(clock_period/2) clk = ~clk;
+	end
+	
+	initial begin
+		clrn = 1'b0; #20;
+		clrn = 1'b1; #20;
+	end
+	
+	wire [7:0] data1;
+	assign data1 = data;
+	
+	ps2_keyboard inst(.clk(clk), .clrn(clrn), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .nextdata_n(nextdata_n), .data(data), .ready(ready), .overflow(overflow));
+		
+
+	bcd7seg b1(.b(data1[3:0]), .h(h1));
+	bcd7seg b2(.b(data1[7:4]), .h(h2));
 
 endmodule
