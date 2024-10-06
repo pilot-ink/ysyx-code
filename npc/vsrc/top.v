bcc10073af260887c13bd67d97ec090f865d3f41
tracer-ysyx <tracer@ysyx.org>
2024-10-06 20:47:22 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:22 up  6:40,  1 user,  load average: 0.00, 0.00, 0.03

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,16 +1,29 @@
-module top(  // 这里名字要与Makefile中的TOPNAME和cpp文件中的#include "Vtop.h"相同
-  input clk,
-  input rst,
-  output reg [15:0] led
+module top(clk,rst,ps2_clk,ps2_data,
+	data,ready,overflow
 );
-  reg [31:0] count;
-  always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]}; // 后四位：0001->0010->0100->1000 为1的位亮灯
-      count <= (count >= 5000000 ? 32'b0 : count + 1); // 控制亮灯间隔
-    end
-  end
+	input clk,rst,ps2_clk,ps2_data;
+	output [7:0] data;
+	output ready;
+	output overflow;
+	output hex0
+	
+	reg [7:0] data_p;
+	reg nextdata_n;
+	
+	
+	ps2_keyboard inst(.clk(clk), .clrn(rst), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .nextdata_n(nextdata_n),.ready(ready), .overflow(overflow));
+	
+	bcd7seg b1(.b(data_p[3:0]), .h(hex0));
+	always @(*) begin
+		if(ready) begin
+		data_p <= data;
+		nextdata_n <= 0;
+		end
+		else
+		nextdata_n <= 1;
+		end
+	end 
+
 endmodule
 
 
