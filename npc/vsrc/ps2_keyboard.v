2a947e71a50e40c80225616e6e66bb3bc656af2b
tracer-ysyx <tracer@ysyx.org>
2024-10-06 15:34:11 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:11 up  1:26,  1 user,  load average: 0.93, 1.02, 0.99

--- npc/vsrc/ps2_keyboard.v
+++ npc/vsrc/ps2_keyboard.v
@@ -1,79 +1,55 @@
-`timescale 1ns / 1ps
 module ps2_keyboard(clk,clrn,ps2_clk,ps2_data,data,
-				ready,nextdata_n,overflow);
+                    ready,nextdata_n,overflow);
+    input clk,clrn,ps2_clk,ps2_data;
+    input nextdata_n;
+    output [7:0] data;
+    output reg ready;
+    output reg overflow;     // fifo overflow
+    // internal signal, for test
+    reg [9:0] buffer;        // ps2_data bits
+    reg [7:0] fifo[7:0];     // data fifo
+    reg [2:0] w_ptr,r_ptr;   // fifo write and read pointers
+    reg [3:0] count;  // count ps2_data bits
+    // detect falling edge of ps2_clk
+    reg [2:0] ps2_clk_sync;
 
-	input clk,clrn,ps2_clk,ps2_data;
-	input nextdata_n;
-	output [7:0] data;
-	output reg ready;
-	output reg overflow;
+    always @(posedge clk) begin
+        ps2_clk_sync <=  {ps2_clk_sync[1:0],ps2_clk};
+    end
 
-	
-	reg [9:0] buffer;
-	reg [7:0] fifo[7:0];
-	reg [2:0] r_ptr,w_ptr;
-	reg [3:0] count;
-	
-	reg [2:0] ps2_clk_sync;
+    wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
 
-	always @(posedge clk) begin
-		ps2_clk_sync <= {ps2_clk_sync[1:0],ps2_clk};
-	end
-
-	wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
-
-	
-	always @(posedge clk) begin
-		if(clrn == 0) begin
-			count <= 0; w_ptr <= 0; r_ptr <= 0; overflow <= 0; ready <=0;
-		end
-		else begin
-			if(ready ) begin
-				if(nextdata_n == 1'b0) begin
-					r_ptr <= r_ptr + 3'b1;
-					if(w_ptr == (r_ptr + 3'b1))
-						ready <= 1'b0;
-				end
-			end
-			if(sampling) begin
-				if(count == 4'b10) begin
-					if((buffer[0] == 0) &&
-						(ps2_data) &&
-						(^buffer[9:1])) begin
-						fifo[w_ptr] <= buffer[8:1];
-						w_ptr <= w_ptr + 3'b1;
-						ready <= 1'b1;
-						overflow <= overflow | (r_ptr == (w_ptr + 3'b1));
-					end
-					count <= 0;
-				end	
-				else begin
-					buffer[count] <= ps2_data;
-					count <= count + 3'b1;
-				end
-			end
-		end
-	end
-
-
-endmodule
-
-
-
-
-module read(
-	input reg ready,
-	output reg nextdata
-);
-	always @(*) begin
-		if(ready) begin
-			nextdata = 1'b0;
-			#20;
-			nextdata = 1'b1;	
-		end
-	end
-
-	
-
+    always @(posedge clk) begin
+        if (clrn == 0) begin // reset
+            count <= 0; w_ptr <= 0; r_ptr <= 0; overflow <= 0; ready<= 0;
+        end
+        else begin
+            if ( ready ) begin // read to output next data
+                if(nextdata_n == 1'b0) //read next data
+                begin
+                    r_ptr <= r_ptr + 3'b1;
+                    if(w_ptr==(r_ptr+1'b1)) //empty
+                        ready <= 1'b0;
+                end
+            end
+            if (sampling) begin
+              if (count == 4'd10) begin
+                if ((buffer[0] == 0) &&  // start bit
+                    (ps2_data)       &&  // stop bit
+                    (^buffer[9:1])) begin      // odd  parity
+                    fifo[w_ptr] <= buffer[8:1];  // kbd scan code
+                    w_ptr <= w_ptr+3'b1;
+                    ready <= 1'b1;
+                    overflow <= overflow | (r_ptr == (w_ptr + 3'b1));
+                end
+                count <= 0;     // for next
+              end else begin
+                buffer[count] <= ps2_data;  // store ps2_data
+                count <= count + 3'b1;
+              end
+            end
+        end
+    end
+    assign data = fifo[r_ptr]; //always set output data
 
 endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,93 +1,66 @@
-`timescale 1ns / 1ps
-module top(
-	output [6:0] h1,h2
-);
-	/* parameter */
-parameter [31:0] clock_period = 10;
+module top(clk, clrn, ps2_clk, ps2_data,flag, flag2,hex0);
 
-/* ps2_keyboard interface signals */
-reg clk,clrn;
-wire [7:0] data;
-wire ready,overflow;
-wire kbd_clk, kbd_data;
-reg nextdata_n;
-wire [7:0] data1;
-assign data1 = data;
+	input clk,clrn,ps2_clk,ps2_data;
+	wire [7:0] data_code;
+	wire ready;
+	wire overflow;
+	reg nextdata_n2;
+	
+	reg [7:0] data_p;
+	reg [7:0] count = 0; // an le duoshao ci
+	
+	output reg flag = 1;   // judge keyboard-button is valid or not
+	output reg flag2 = 1;
+	output reg [6:0] hex0;
 
-ps2_keyboard_model model(
-    .ps2_clk(kbd_clk),
-    .ps2_data(kbd_data)
-);
-
-ps2_keyboard inst(
-    .clk(clk),
-    .clrn(clrn),
-    .ps2_clk(kbd_clk),
-    .ps2_data(kbd_data),
-    .data(data),
-    .ready(ready),
-    .nextdata_n(nextdata_n),
-    .overflow(overflow)
-);
-
-initial begin /* clock driver */
-    clk = 0;
-    forever
-        #(clock_period/2) clk = ~clk;
-end
-
-initial begin
-    clrn = 1'b0;  #20;
-    clrn = 1'b1;  #20;
-    model.kbd_sendcode(8'h1C); // press 'A'
-    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1;//read data
-    model.kbd_sendcode(8'hF0); // break code
-    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
-    model.kbd_sendcode(8'h1C); // release 'A'
-    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
-    model.kbd_sendcode(8'h1B); // press 'S'
-    #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
-    #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
-    model.kbd_sendcode(8'hF0); // break code
-    model.kbd_sendcode(8'h1B); // release 'S'
-    #20;
-    $stop;
-end
-
-	bcd7seg b1(.b(data1[3:0]), .h(h1));
-	bcd7seg b2(.b(data1[7:4]), .h(h2));
-
-endmodule
+	reg [6:0] count_clk = 0;
+	reg clk_b = 0;
+	always @(posedge clk)
+	begin
+		if(count_clk == 100)
+		begin
+			count_clk <= 0;
+			clk_b <= ~clk_b;
+		end
+		else
+			count_clk <= count_clk + 1;
+	end
 
 
+	ps2_keyboard S(.clk(clk), .clrn(clrn), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .data(data_code), .ready(ready), .nextdata_n(nextdata_n2), .overflow(overflow));
 
+	bcd7seg b1(.b(data_p[3:0]), .h(hex0));
 
-module ps2_keyboard_model(
-    output reg ps2_clk,
-    output reg ps2_data
-    );
-parameter [31:0] kbd_clk_period = 60;
-initial ps2_clk = 1'b1;
+	always @(posedge clk_b)
+	begin
+		if (ready)
+		begin
+			if(data_code != 8'hf0 && flag2)
+			begin
+				data_p <= data_code;
+				flag2 <= 1;
+				flag <= 1;
+			end
+			else if (data_code == 8'hf0)
+			begin
+				data_p <= data_code;
+				count <= count + 1;
+				flag <= 0;
+				flag2 <= 0;
+			end
+			else if(!flag2)
+			begin
+				data_p <= data_code;
+				flag2 <= 1;
+				flag <= 0;
+			end
 
-task kbd_sendcode;
-    input [7:0] code; // key to be sent
-    integer i;
-
-    reg[10:0] send_buffer;
-    begin
-        send_buffer[0]   = 1'b0;  // start bit
-        send_buffer[8:1] = code;  // code
-        send_buffer[9]   = ~(^code); // odd parity bit
-        send_buffer[10]  = 1'b1;  // stop bit
-        i = 0;
-        while( i < 11) begin
-            // set kbd_data
-            ps2_data = send_buffer[i];
-            #(kbd_clk_period/2) ps2_clk = 1'b0;
-            #(kbd_clk_period/2) ps2_clk = 1'b1;
-            i = i + 1;
-        end
-    end
-endtask
+			nextdata_n2 <= 0;
+		end
+		else
+		begin
+			nextdata_n2 <= 1;
+		end
+	end
 
 endmodule
