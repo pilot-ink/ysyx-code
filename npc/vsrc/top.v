48689db494e56e54dc506e63d77ca69153667362
tracer-ysyx <tracer@ysyx.org>
2024-10-06 14:15:56 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:15:56 up 8 min,  1 user,  load average: 0.98, 0.56, 0.24

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,12 +1,15 @@
+`timescale 1ns / 1ps
 module top(
-     input clk,clrn,ps2_clk,ps2_data,
+     input clrn,ps2_clk,ps2_data,
      input nextdata_n,  
      output [7:0] data,
      output reg ready,
      output reg overflow,
      output [6:0] h1,h2
 );
-	
+	/* parameter */
+	parameter [31:0] clock_period = 10;
+	reg clk;
 	wire [7:0] data1;
 	assign data1 = data;
 	
@@ -15,5 +18,10 @@ module top(
 
 	bcd7seg b1(.b(data1[3:0]), .h(h1));
 	bcd7seg b2(.b(data1[7:4]), .h(h2));
+	initial begin /* clock driver */
+    		clk = 0;
+    		forever
+        	#(clock_period/2) clk = ~clk;
+	end
 
 endmodule
