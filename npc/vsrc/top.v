2676d3809b15dc83ff001dfc0a3c08d2f2766951
tracer-ysyx <tracer@ysyx.org>
2024-10-06 20:52:11 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:11 up  6:44,  1 user,  load average: 0.05, 0.02, 0.02

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -14,13 +14,13 @@ module top(clk,rst,ps2_clk,ps2_data,
 	ps2_keyboard inst(.clk(clk), .clrn(rst), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .data(data),.nextdata_n(nextdata_n),.ready(ready), .overflow(overflow));
 	
 	bcd7seg b1(.b(data_p[3:0]), .h(hex0));
-	always @(*) begin
+	always @(posedge clk) begin
 		if(ready) begin
-		data_p = data;
-		nextdata_n = 0;
+		data_p <= data;
+		nextdata_n <= 0;
 		end
 		else begin
-		nextdata_n = 1;
+		nextdata_n <= 1;
 		end
 	end 
 
