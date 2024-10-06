829256a6482b4e906802140b4984801f6e348e45
tracer-ysyx <tracer@ysyx.org>
2024-10-06 17:34:00 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:34:00 up  3:26,  1 user,  load average: 0.01, 0.08, 0.22

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,66 +1,16 @@
-module top(clk, clrn, ps2_clk, ps2_data,flag, flag2,hex0,ready);
-
-	input clk,clrn,ps2_clk,ps2_data;
-	wire [7:0] data_code;
-	wire overflow;
-	reg nextdata_n2;
-	output ready;
-	
-	reg [7:0] data_p;
-	reg [7:0] count = 0; // an le duoshao ci
-	
-	output reg flag = 1;   // judge keyboard-button is valid or not
-	output reg flag2 = 1;
-	output reg [6:0] hex0;
-
-	reg [6:0] count_clk = 0;
-	reg clk_b = 0;
-	always @(posedge clk)
-	begin
-		if(count_clk == 100)
-		begin
-			count_clk <= 0;
-			clk_b <= ~clk_b;
-		end
-		else
-			count_clk <= count_clk + 1;
-	end
-
-
-	ps2_keyboard S(.clk(clk), .clrn(clrn), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .data(data_code), .ready(ready), .nextdata_n(nextdata_n2), .overflow(overflow));
-
-	bcd7seg b1(.b(data_p[3:0]), .h(hex0));
-
-	always @(posedge clk_b)
-	begin
-		if (ready)
-		begin
-			if(data_code != 8'hf0 && flag2)
-			begin
-				data_p <= data_code;
-				flag2 <= 1;
-				flag <= 1;
-			end
-			else if (data_code == 8'hf0)
-			begin
-				data_p <= data_code;
-				count <= count + 1;
-				flag <= 0;
-				flag2 <= 0;
-			end
-			else if(!flag2)
-			begin
-				data_p <= data_code;
-				flag2 <= 1;
-				flag <= 0;
-			end
-
-			nextdata_n2 <= 0;
-		end
-		else
-		begin
-			nextdata_n2 <= 1;
-		end
-	end
-
+module top(  // 这里名字要与Makefile中的TOPNAME和cpp文件中的#include "Vtop.h"相同
+  input clk,
+  input rst,
+  output reg [15:0] led
+);
+  reg [31:0] count;
+  always @(posedge clk) begin
+    if (rst) begin led <= 1; count <= 0; end
+    else begin
+      if (count == 0) led <= {led[14:0], led[15]}; // 后四位：0001->0010->0100->1000 为1的位亮灯
+      count <= (count >= 5000000 ? 32'b0 : count + 1); // 控制亮灯间隔
+    end
+  end
 endmodule
+
+
