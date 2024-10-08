e7f4e9575736cf1560d82a848451e3b644ef3ad4
tracer-ysyx <tracer@ysyx.org>
2024-10-08 14:02:58 +0800

    > compile NEMU ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:58 up 17:22,  4 users,  load average: 0.43, 0.31, 0.26

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,7 +54,8 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 static int cmd_si(char *args){
-  cpu_exec(atoi(args));
+  if(args == NULL) cpu_exec(1);
+  else cpu_exec(atoi(args));
   return 0;
 }
 
