ff2e2a4b4ac6d66a253b1c5be99ab94a10790198
tracer-ysyx <tracer@ysyx.org>
2024-10-11 11:46:00 +0800

    > compile NEMU ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:46:00 up 1 day,  1:55,  1 user,  load average: 1.07, 1.18, 0.88

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -93,14 +93,14 @@ static bool make_token(char *e) {
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        printf("1\n");
+        
         position += substr_len;
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+printf("1\n"); 
         switch (rules[i].token_type) {
           case(TK_NOTYPE): continue;
           case(COMPUTE): 
@@ -113,8 +113,8 @@ static bool make_token(char *e) {
           default: ;
         }
         pos++;
-
-        //break;
+printf("2\n");
+        break;
       }
     }
 
