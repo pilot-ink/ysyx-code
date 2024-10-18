d644b2e9485928de38b101616414ee45e5a481b2
dhb <2193088043@qq.com>
2024-10-18 16:38:04 +0800

    完成流水灯实验,  修改.gitignore

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,17 +1,8 @@
-
-/*线性反馈移位寄存器*/
 module top(
 	input clk,rst,
-  input [1:0] data[3:0],
-  input [1:0] choose,
-	output [1:0] result
+  output [15:0] led
 );
-	MuxKey #(4, 2, 2) i0 (result , choose, {
-    2'b00, data[0],
-    2'b01, data[1],
-    2'b10, data[2],
-    2'b11, data[3]
-  });
+	water_lamp w1 (.clk(clk), .rst(rst), .led(led));
 
 endmodule
 
