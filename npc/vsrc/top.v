671d22921fec9e0b89e6f91ba50621c6b0bd8baf
tracer-ysyx <tracer@ysyx.org>
2024-10-18 11:52:10 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:10 up 3 days,  2:10,  1 user,  load average: 0.00, 0.00, 0.09

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,42 +1,23 @@
 
 /*线性反馈移位寄存器*/
-// module top(
-// 	input clk,rst,
-// 	input clock,
-// 	output [7:0] result,
-// 	output [6:0] hex,hex1
-// );
-// 	wire [3:0] h,h1;
-// 	assign h = result[3:0];
-// 	assign h1 = result[7:4];
-
-// 	LFSR l1(.clock(clock), .result(result));
-// 	bcd7seg b1(.b(h), .h(hex));
-// 	bcd7seg b2(.b(h1), .h(hex1));
-
-// endmodule
-
-
-/*ALU*/
 module top(
-  input clk,rst,
-  input  wire [3:0] A,
-  input  wire [3:0] b,
-  input  wire [2:0] option,
-  output reg        carry,
-  output reg        overflow,
-  output reg        zero,
-  output reg  [3:0] result,
-  output 	  [6:0] hex
+	input clk,rst,
+	input clock,
+	output [7:0] result,
+	output [6:0] hex,hex1
 );
+	wire [3:0] h,h1;
+	assign h = result[3:0];
+	assign h1 = result[7:4];
 
-	wire [3:0] h;
-	assign h = result;
-
-	ALU4 a1(.A(A), .b(b), .option(option), .carry(carry), .overflow(overflow), .zero(zero), .result(result));
+	LFSR l1(.clock(clock), .result(result));
 	bcd7seg b1(.b(h), .h(hex));
+	bcd7seg b2(.b(h1), .h(hex1));
 
 endmodule
+
+
+
 
 
 
