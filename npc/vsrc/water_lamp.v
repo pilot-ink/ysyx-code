29cb9703a4978e44dcbe7d8a9e9660f7328245f0
tracer-ysyx <tracer@ysyx.org>
2024-10-21 11:13:59 +0800

    > sim RTL ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:13:59 up  2:00,  1 user,  load average: 1.10, 0.57, 0.29

--- npc/vsrc/water_lamp.v
+++ npc/vsrc/water_lamp.v
@@ -9,7 +9,7 @@ module water_lamp(  // 这里名字要与Makefile中的TOPNAME和cpp文件中的
     if (rst) begin led <= 1; count <= 0; end
     else begin
       if (count == 0) led <= {led[14:0], led[15]}; // 后四位：0001->0010->0100->1000 为1的位亮灯
-      count <= (count >= 5000000 ? 32'b0 : count + 1); // 控制亮灯间隔
+      count <= (count >= 5000000 ? 32'b0 : count + 10); // 控制亮灯间隔
     end
   end
 endmodule
