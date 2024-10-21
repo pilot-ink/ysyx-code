73bbe241a5834a3aae640eda7749c16e305396ea
tracer-ysyx <tracer@ysyx.org>
2024-10-21 16:27:10 +0800

    > compile NEMU ysyx_20240911 Tom Linux dhb-virtual-machine 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:10 up  7:13,  1 user,  load average: 0.15, 0.33, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -342,14 +342,14 @@ word_t expr(char *e, bool *success) {
 
   /*check whether expr is right*/
   //check_parentheses(0, nr_token);
-  if(atoi(tokens[0].str) != eval(1,nr_token-1)){
-  printf("expr:\n");
-  printf("info:total:%d\n",nr_token-1);
-  for(i = 1; i < nr_token; i++)
-    printf("%s",tokens[i].str);
-  printf("\n");
-  printf("result:%s\tresult:%u\n",tokens[0].str,eval(1,nr_token-1));
-  }
+  // if(atoi(tokens[0].str) != eval(1,nr_token-1)){
+  // printf("expr:\n");
+  // printf("info:total:%d\n",nr_token-1);
+  // for(i = 1; i < nr_token; i++)
+  //   printf("%s",tokens[i].str);
+  // printf("\n");
+  // printf("result:%s\tresult:%u\n",tokens[0].str,eval(1,nr_token-1));
+  // }
   //printf("result:%d\n",check_parentheses(1, nr_token));
   
   //printf("expr's value is %d",eval(0, nr_token));
@@ -360,5 +360,5 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
 
-  //return eval(0,nr_token-1);
+  return eval(0,nr_token-1);
 }
