eccdaf7ffc3e395af7031c506811eaef21753ceb
tracer-ysyx <tracer@ysyx.org>
2024-11-07 11:03:12 +0800

    > sim RTL ysyx_24100027 dhb Linux dhb-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:12 up 19 min,  1 user,  load average: 0.23, 0.16, 0.14

--- npc/vsrc/ysyx_24100027_IDU.v
+++ npc/vsrc/ysyx_24100027_IDU.v
@@ -5,7 +5,6 @@ module ysyx_24100027_IDU(
     output Regwrite,
     output [2:0] immtype
 );
-    wire [6:0] opcode;
     assign Regwrite = 1;
     assign immtype = 3'b000;
     assign alucontrol = 3'b000;
