226f59ec718476f65a3281b6bdfdeb01bac04919
tracer-ysyx <tracer@ysyx.org>
2024-11-12 14:58:23 +0800

    > compile NEMU ysyx_24100027 dhb Linux dhb-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:58:23 up 3 days, 0 min,  1 user,  load average: 0.15, 0.17, 0.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,9 +39,11 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+#ifdef CONFIG_WATCHPOINT
   bool change = false;
   change = watchpoint_check();
   if(change == true) nemu_state.state = NEMU_STOP;
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
