4db0d43f83eb12065fbc4603b4d9560f830dace5
tracer-ysyx <tracer@ysyx.org>
2024-12-02 15:42:42 +0800

    > compile NEMU ysyx_24100027 dhb Linux dhb-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:42 up 3 days,  5:40,  1 user,  load average: 0.25, 0.32, 0.28

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,8 +20,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   printf("%x\n",pc);
-  if(ref_r->pc != pc) 
-    return false;
+  
    for(int i = 0; i < 32; i++)
   {
     if(cpu.gpr[i] != ref_r->gpr[i])
