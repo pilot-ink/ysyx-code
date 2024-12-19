2aa41e5e4c0fb330a7fd7d986741d0a0d8fbeba1
tracer-ysyx <tracer@ysyx.org>
2024-12-19 14:43:17 +0800

    > compile NEMU ysyx_24100027 dhb Linux dhb-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:43:17 up 2 days, 58 min,  1 user,  load average: 0.63, 0.55, 0.40

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,6 +19,7 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  printf("herediff\n");
   uint8_t *ptr = (uint8_t *)buf;
   if(direction == DIFFTEST_TO_DUT){ //to npc
     *ptr = paddr_read(addr,n);
