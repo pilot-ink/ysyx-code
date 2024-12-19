771044667d65b287450f025fa7ae20eb3bfd1f02
tracer-ysyx <tracer@ysyx.org>
2024-12-19 14:50:40 +0800

    > compile NEMU ysyx_24100027 dhb Linux dhb-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:40 up 2 days,  1:05,  1 user,  load average: 0.42, 0.58, 0.48

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -65,7 +65,8 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data);printf("asdasd\n"); return; }
+  printf("asdasd\n");
+  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
