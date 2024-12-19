6d3086c32cfb5a4742074bc662d42d2752fff3fd
tracer-ysyx <tracer@ysyx.org>
2024-12-19 14:39:37 +0800

    > compile NEMU ysyx_24100027 dhb Linux dhb-virtual-machine 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:39:37 up 2 days, 54 min,  1 user,  load average: 0.51, 0.44, 0.35

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -25,7 +25,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
   }
   else { //to nemu
     for(int i = 0; i < n; i++)
-      paddr_write(addr, 1, (word_t)ptr[i]);
+      paddr_write(addr+i, 1, (word_t)ptr[i]);
   }
 }
 
